2836ff651fe7c2b7838602f9976e3e6539347ea0
yuluyan <luyan.yu@utexas.edu>
2020-04-02 10:23:11 -0500

    update dependency

--- src/js/jazzify-core.js
+++ src/js/jazzify-core.js
@@ -1,6 +1,20 @@
+import abcjs from "abcjs"
+
 let Jazzify = function (scoreString) {
     console.log("Jazzify")
-    alert("Coming soon!")
+    let parseObj = abcjs.parseOnly(scoreString)[0]
+    console.log(parseObj)
+    console.log(parseObj.getBarLength())
+    console.log(parseObj.getBeatLength())
+    console.log(parseObj.getBeatsPerMeasure())
+    console.log(parseObj.getBpm())
+    console.log(parseObj.getMeter())
+    console.log(parseObj.getMeterFraction())
+    console.log(parseObj.getPickupLength())
+    console.log(parseObj.getKeySignature())
+
+    parseObj.lines
+
     return scoreString
 }
 
